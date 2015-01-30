375abc636b08b4b83e4744fa5def3073e5b33324
Jordan Danford <jordandanford@gmail.com>
2015-01-29 17:50:54 -0700

    Add React presentation

---
+++ react/js/init.js
@@ -0,0 +1,46 @@
+function highlightCode() {
+  var nodes = document.querySelectorAll("script[type='text/code']");
+  for (var i = 0; i < nodes.length; i++) {
+    var script = nodes[i];
+    var text = script.text
+      .trim()
+      .replace(/&/g,"&amp;")
+      .replace(/</g,"&lt;")
+      .replace(/>/g,"&gt;");
+
+    var pre = document.createElement("pre");
+    pre.className = script.className;
+    var code = document.createElement("code");
+    code.innerHTML = text;
+    pre.appendChild(code);
+
+    script.parentElement.replaceChild(pre, script);
+    hljs.configure({
+      languages: ["javascript", "html"]
+    });
+    hljs.highlightBlock(pre);
+  }
+}
+
+Reveal.initialize({
+  width: 1000,
+  height: 750,
+  margin: 0.05,
+  controls: false,
+  progress: true,
+  history: true,
+  center: false,
+
+  theme: Reveal.getQueryHash().theme || 'danford', // available themes are in /css/theme
+  transition: Reveal.getQueryHash().transition || 'none', // default/cube/page/concave/zoom/linear/fade/none
+
+  dependencies: [
+    { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    // { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    // { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    //{ src: 'plugin/highlight/highlight.js', async: true, callback: highlightCode },
+    { src: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js', async: true, callback: highlightCode },
+    { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+    // { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+  ]
+});
